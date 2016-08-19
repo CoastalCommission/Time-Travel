4317742680cbb587c35265b08df955c8dc371dbf
Zachariah Moreno <xachmoreno@gmail.com>
2016-08-19 13:15:44 -0700

    Rendering events from events.json

--- main.js
+++ main.js
@@ -15,7 +15,7 @@
       win.loadURL(`file://${__dirname}/index.html`)
 
       // Open the DevTools.
-    //   win.webContents.openDevTools({mode: 'undocked'})
+      win.webContents.openDevTools({mode: 'undocked'})
 
       // Emitted when the window is closed.
       win.on('closed', () => {
