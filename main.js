b4f04875c62c6d63b9e3679191ab63d563f1953c
Zachariah Moreno <xachmoreno@gmail.com>
2016-08-18 20:16:12 -0700

    :koala: Fixing jQuery loading issue running in CommonJS env

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 (function() {
-    // 'use strict';
+    'use strict';
 
     const {app, BrowserWindow} = require('electron')
 
@@ -9,13 +9,13 @@
 
     function createWindow () {
       // Create the browser window.
-      win = new BrowserWindow({width: 800, height: 600})
+      win = new BrowserWindow({width: 840, height: 755})
 
       // and load the index.html of the app.
       win.loadURL(`file://${__dirname}/index.html`)
 
       // Open the DevTools.
-      win.webContents.openDevTools()
+    //   win.webContents.openDevTools({mode: 'undocked'})
 
       // Emitted when the window is closed.
       win.on('closed', () => {
